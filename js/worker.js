eb2d4bc59983e7b06a563e83d7c8c7d54f050194
Joachim Cardoen <diff@Joachims-MacBook-Pro.local>
2018-03-20 23:50:11 +0100

    updated worker

--- js/worker.js
+++ js/worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'restaurants-cache-v2';
+const cacheName = 'restaurants-cache-v3';
 
 // Listen for install event, set callback
 self.addEventListener('install', function (event) {
@@ -7,12 +7,23 @@ self.addEventListener('install', function (event) {
         caches.open(cacheName).then(function (cache) {
             return cache.addAll(
                 [
-                    '/css/*',
-                    '/img/*',
-                    '/js/*',
-                    '/data/*',
+                    '/css/styles.css',
+                    '/data/restaurants.json',
+                    '/js/dbhelper.js',
+                    '/js/main.js',
+                    '/js/restarurant_info.js',
+                    '/restarurant.html',
+                    'img/1.jpg',
+                    'img/2.jpg',
+                    'img/3.jpg',
+                    'img/4.jpg',
+                    'img/5.jpg',
+                    'img/6.jpg',
+                    'img/7.jpg',
+                    'img/8.jpg',
+                    'img/9.jpg',
+                    'img/10.jpg',
                     '/index.html',
-                    '/restaurant.html'
                 ]
             );
         })
